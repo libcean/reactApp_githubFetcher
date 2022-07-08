bed1fb0cf6118d2bf9d8715c20490219b3edcc1f
SAMUEL Omaiye <omaiyesamuel@gmail.com>
2022-07-08 03:44:31 +0100

    Updates

---
+++ precache-manifest.14e776ac5715b625bd8ed571c5c52098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0122068a84af51e7a59b647c5f755b86",
+    "url": "/reactApp_githubFetcher/index.html"
+  },
+  {
+    "revision": "6d53b8358df1409db75d",
+    "url": "/reactApp_githubFetcher/static/css/main.51b1ab87.chunk.css"
+  },
+  {
+    "revision": "df2a2dc809350ba4f331",
+    "url": "/reactApp_githubFetcher/static/js/2.d6a34602.chunk.js"
+  },
+  {
+    "revision": "f11caf86a100a3f2d7a7e098868de1a9",
+    "url": "/reactApp_githubFetcher/static/js/2.d6a34602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d53b8358df1409db75d",
+    "url": "/reactApp_githubFetcher/static/js/main.03a2fefa.chunk.js"
+  },
+  {
+    "revision": "493c3f0202dd2780255a",
+    "url": "/reactApp_githubFetcher/static/js/runtime-main.90871608.js"
+  }
+]);
